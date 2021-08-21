bad5a343aab71b87df6fcab5951897859b736572
jotan7 <jotan7@up.edu.ph>
2021-08-21 19:06:08 +0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "frame-gray": "#F3F6FE",
+        "main-gray": "#28394F",
+      },
+      fontFamily: {
+        "DM-Sans": "my-font",
+        Roboto: "Roboto",
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
